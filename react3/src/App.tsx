b63ed65e2091cd06c09520f93857dc484e47f512
1voodoo <pixel0101@mail.ru>
2022-02-08 11:38:28 +0300

    FE61-52

--- react3/src/App.tsx
+++ react3/src/App.tsx
@@ -1,11 +1,44 @@
-import { FC } from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { FC, useEffect, useState } from 'react';
+import style from './App.module.scss';
+import getAllCountries, { IBaseAllCountry } from './components/api/getAllCountries';
 
 const App: FC = () => {
+  const [feth, setFeth] = useState<IBaseAllCountry[] | null>(null);
  
+  const getFetc = async() => {
+    const answer = await getAllCountries();
+    setFeth(answer);
+  };
+
+  useEffect(() => {
+    getFetc();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[]);
+
+  const handleChange = () => {
+    setFeth(feth);
+  };
+  console.log(feth?.map(info => info.name));
   return (
-    <div>
-      Hello Lol
-    </div>
+    <div className={style.conteiner}>
+  <FormControl fullWidth>
+  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+  <Select
+    labelId="demo-simple-select-label"
+    id="demo-simple-select"
+    value={feth}
+    label="Age"
+    onChange={handleChange}
+  >
+    <MenuItem value={10}>Ten</MenuItem>
+    <MenuItem value={20}>Twenty</MenuItem>
+    <MenuItem value={30}>Thirty</MenuItem>
+  </Select>
+</FormControl>
+{/* <div>{feth!.map((info) => (<p>{info.name}</p>
+  ))} </div> */}
+  </div>
   );
 }
 
